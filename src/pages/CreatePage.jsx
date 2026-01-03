fd3c7e156786373801345eba920f7d1009ba6ea5
Dipesh Gavit <dipeshgavit333@gmail.com>
2026-01-04 00:00:06 +0530

    Create Note page

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,7 +1,46 @@
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+
 const CreatePage = () => {
+    const [newNote, setNewNote] = useState({
+        title: "",
+        description: ""
+    });
+
+    const handleCreateNote = () => {
+        console.log(newNote);
+        
+    }
     return (
         <>
-            <div>Create Page</div>
+            <Container maxW={Container.sm}>
+                <VStack spacing={8}>
+                    <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create new Product</Heading>
+                    <Box 
+                        w={"full"} bg={useColorModeValue("white", "gray-800")}
+                        p={6} rounded={"lg"} shadow={"md"}
+                    >
+                        <VStack spacing={4}>
+                            <Input 
+                                placeholder="Title" 
+                                name="title" 
+                                value={newNote.title}
+                                onChange={(e) => setNewNote({...newNote, title: e.target.value})}
+                            />
+                            <Textarea 
+                                placeholder="Description" 
+                                name="description" 
+                                value={newNote.description}
+                                onChange={(e) => setNewNote({...newNote, description: e.target.value})}
+                            />
+
+                            <Button colorScheme="green" onClick={handleCreateNote} w={"full"}>
+                                Create Note
+                            </Button>
+                        </VStack>
+                    </Box>
+                </VStack>
+            </Container>
         </>
     )
 }
