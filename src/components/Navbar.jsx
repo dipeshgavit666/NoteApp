b4c84894c0499bf220e2c247920e1e33dcc27ed6
Dipesh Gavit <dipeshgavit333@gmail.com>
2026-01-03 23:07:07 +0530

    Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
-import { Container, Button} from "@chakra-ui/react";
-import { CiSquarePlus } from "react-icons/ci";
+import { Container,Flex, Button, HStack, Link, Text, useColorMode} from "@chakra-ui/react";
+import { CiSquarePlus} from "react-icons/ci";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
+
 const Navbar = () => {
+
+    const { colorMode, toggleColorMode } = useColorMode()
+
     return(
         <>
             <Container maxW={"1140px"} px={4}>
@@ -14,7 +21,7 @@ const Navbar = () => {
                     }}
                 >
                     <Text
-                        bgGradient='linear(to-l, #7928CA, #FF0080)'
+                        bgGradient='linear(to-l, #226802ff, #28ca30ff)'
                         bgClip='text'
                         fontSize='6xl'
                         fontWeight='extrabold'
@@ -22,13 +29,16 @@ const Navbar = () => {
                     <Link to={"/"}>Notes</Link>
                     </Text>
 
-                    <Hstack spacing={2} alignItems={"center"}>
-                        <LInk to={"/create"}>
+                    <HStack spacing={2} alignItems={"center"}>
+                        <Link to={"/create"}>
                             <Button>
-                                <CiSquarePlus />
+                                <CiSquarePlus  fontSize={20} />
                             </Button>
-                        </LInk>
-                    </Hstack>
+                        </Link>
+                        <Button onClick={toggleColorMode}>
+                            {colorMode === "dark" ? <LuSun /> : <IoMoon />}
+                        </Button>
+                    </HStack>
                 </Flex>
             </Container>
         </>
